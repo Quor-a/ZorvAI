eeeb1a23f176ec22f0186540c4ff9b45393ea20f
Gu <gu@quro.ai>
2026-08-20 22:33:49 +0800

    Bump version to 1.0.61 (497)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ai.assistance.quro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 496
-        versionName = "1.0.60"
+        versionCode = 497
+        versionName = "1.0.61"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
